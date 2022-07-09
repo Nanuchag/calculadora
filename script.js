9db53166ae7cb63ae9222c4385b9c2566776f5ac
Nanu Guiralde <nanuguiralde@gmail.com>
2022-07-09 16:04:16 -0300

    se agrega funciones a los botones

--- script.js
+++ script.js
@@ -2,5 +2,80 @@
 //document.querySelector().innerText=''
 
 function agregarNumeroSiete(){
-    document.querySelector().innerText +='7'
+    document.querySelector('.display').innerText +='7'
+}
+
+function agregarLetraC(){
+    document.querySelector('.display').innerText =''
+}
+
+function agregarLetraCE(){
+    document.querySelector('.display').innerText +=''
+}
+
+function agregarPorcentaje(){
+    document.querySelector('.display').innerText +='%'
+}
+
+function agregarDividir(){
+    document.querySelector('.display').innerText +='÷'
+}
+
+function agregarNumeroOcho(){
+    document.querySelector('.display').innerText +='8'
+}
+
+function agregarNumeroNueve(){
+    document.querySelector('.display').innerText +='9'
+}
+
+function agregarMulti(){
+    document.querySelector('.display').innerText +='x'
+}
+
+function agregarNumeroCuatro(){
+    document.querySelector('.display').innerText +='4'
+}
+function agragarNumeroCinco(){
+    document.querySelector('.display').innerText +='5'
+}
+
+function agregarNumeroSeis(){
+    document.querySelector('.display').innerText +='6'
+}
+
+function agregarResta(){
+    document.querySelector('.display').innerText +='-'
+}
+
+function agregarNumeroUno(){
+    document.querySelector('.display').innerText +='1'
+}
+
+function agregarNumeroDos(){
+    document.querySelector('.display').innerText +='2'
+}
+
+function agregarNumeroTres(){
+    document.querySelector('.display').innerText +='3'
+}
+
+function agregarSumar(){
+    document.querySelector('.display').innerText +='+'
+}
+
+function agregarNumeroCero(){
+    document.querySelector('.display').innerText +='0'
+}
+
+function agregarDobleCero(){
+    document.querySelector('.display').innerText +='00'
+}
+
+function agregarPunto(){
+    document.querySelector('.display').innerText +='.'
+}
+
+function agregarIgual(){
+    document.querySelector('.display').innerText +='='
 }
